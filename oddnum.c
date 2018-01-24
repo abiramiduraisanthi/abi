e4e45aa1228f94505ee14b468d1771dea19f0754
abiramiduraisanthi <31506090+abiramiduraisanthi@users.noreply.github.com>
2018-01-24 17:01:46 +0530

    Rename odd.c to oddnum.c

---
+++ oddnum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int f,l,i;
+printf("\n enter the two intervels");
+scanf("%d%d",&f,&l);
+for(i=f;i<=l;i++)
+{
+if(i%2==1)
+{
+printf("%d\n enter the intervels is odd number",i);
+}
+else
+{
+printf("%d\n enter the tw intervels is even",i);
+}
+}
+return 0;
+}
