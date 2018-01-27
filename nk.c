4cb78703c950ac6a10bb3c8a71fc57f351052abd
abiramiduraisanthi <31506090+abiramiduraisanthi@users.noreply.github.com>
2018-01-27 16:34:40 +0530

    Create nk.c

---
+++ nk.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int N,K,sum=0,a[8];
+printf("\n enter the values");
+scanf("%d%d",&N,&K);
+for(i=1;i<=N;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=1;i<=k;i++)
+{
+sum=sum+a[i];
+printf("%d",sum);
+}
+return 0;
