335ac7cc5791d3b846772f1a974393867d0ab29c
abiramiduraisanthi <31506090+abiramiduraisanthi@users.noreply.github.com>
2018-01-31 16:13:16 +0530

    Update Arm.c

--- Arm.c
+++ Arm.c
@@ -1,24 +1,23 @@
 #include<stdio.h>
-#include<conio.h> 
 int main()
 {
-int n,t, rem,result=0;
-printf(\n enter the number:"):
-scanf("%d"'&n);
+int n,t,rem,result=0;
+printf("\n enter the number:");
+scanf("%d",&n);
 t=n;
 while(n!=0)
 {
-rem=n℅10;
+rem=n%10;
 result=result+rem*rem*rem;
 n=n/10;
 }
 if(t==result)
 {
-Prinf("%d" is Armstrong number",n);
+prinf("%d is Armstrong number:",n);
 }
 else
 {
-printf("%d is not Armstrong number",n);
+printf("%d is not Armstrong number:",n);
 }
 return 0;
 }
