9e8a1971b2557e3e0b2a34d6ddfd9eddc409877f
abiramiduraisanthi <31506090+abiramiduraisanthi@users.noreply.github.com>
2018-01-31 16:46:10 +0530

    Update Arm.c

--- Arm.c
+++ Arm.c
@@ -2,22 +2,22 @@
 int main()
 {
 int n,t,rem,result=0;
-printf("\n enter n value:");
+printf("\n enter the n value:");
 scanf("%d",&n);
 t=n;
 while(t!=0)
 {
-rem=n%10;
+rem=t%10;
 result=result+rem*rem*rem;
 t=t/10;
 }
-if(result=n)
+if(result==n)
 {
-prinf("%d is Armstrong number:",n);
+printf("%d\n is Armstrong number:",n);
 }
 else
 {
-printf("%d is not Armstrong number:",n);
+printf("%d\n is not Armstrong number:",n);
 }
 return 0;
 }
