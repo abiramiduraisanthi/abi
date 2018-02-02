fff39789ea4eb7fe05c1a9300ef3b0034f6f270f
abiramiduraisanthi <31506090+abiramiduraisanthi@users.noreply.github.com>
2018-02-02 16:15:31 +0530

    Update NK.c

--- NK.c
+++ NK.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
 int main()
 {
-int n,k,a[3],sum=0;
-scanf("%d",&n,&k);
-for(i=1;i<=5;i++)
+int n,k,a[5],i,j,sum=0;
+printf("\n enter the no of element in array");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
 {
 scanf("%d",&a[i]);
 }
-for(i=k;i<=k;i++)
+  printf("\n enter the no of element to be added");
+  scanf("%d",&k);
+for(j=0;j<=k;j++)
 {
 sum=sum+a[i];
 }
-printf("%d",sum);
+printf("%d\n the sum of value is",sum);
 return 0;
 }
