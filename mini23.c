265f72b6bea40308c9715cacd0e7d4d2397d9e82
abiramiduraisanthi <31506090+abiramiduraisanthi@users.noreply.github.com>
2018-02-09 16:23:35 +0530

    Create mini23.c

---
+++ mini23.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int n,a[100],i,temp;
+printf("\n enter the count:");
+scanf("%d",&n);
+printf("\n enter the elements:");
+scanf("%d",&a[i]);
+temp=a[i];
+for(i=2;i<=n;i++)
+{
+scanf("%d",&a[1]);
+}
+if(a[i]<=temp)
+{
+temp=a[i];
+}
+}
+printf("%d\n",temp);
+return 0;
+}
