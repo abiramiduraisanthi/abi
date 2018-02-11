9cf9f863996f9f33838e6bd2d8004279b5f94b72
abiramiduraisanthi <31506090+abiramiduraisanthi@users.noreply.github.com>
2018-02-11 21:15:12 +0530

    Create Max22.c

---
+++ Max22.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n,k,i,a[100];
+printf("\n enter the count:");
+scanf("%d",&n);
+printf("\n enter the elements of array:");
+scanf("%d",&a[i];
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+if(i>=a[i])
+{
+k=a[i];
+}
+printf("%d\n",k);
+return 0
+}
