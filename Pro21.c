3e9dca98a310eb56484f2e9c8cc59e516d2035ce
abiramiduraisanthi <31506090+abiramiduraisanthi@users.noreply.github.com>
2018-02-11 21:53:32 +0530

    Create Pro21.c

---
+++ Pro21.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int i,j,n=0,t,sum=0;
+printf("\n enter the first element:");
+scanf("%d",&a)
+printf("\n enter the difference \n enter the count:")
+scanf("%d%d",&d,&n);
+for(i=0;i<=n;i++)
+{
+if(t==n)
+{
+sum=sum+i;
+}
+printf("%d",sum);
+}
+return 0;
+}
