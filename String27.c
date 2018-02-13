582888852e9068eb1e6ab3f3ad3dc78b181734df
abiramiduraisanthi <31506090+abiramiduraisanthi@users.noreply.github.com>
2018-02-13 22:40:12 +0530

    Create String27.c

---
+++ String27.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+char n;
+printf("\n enter the string element:");
+scanf("%d",&n);
+if((n>=65&&n<=90)<||(n>=97&& n<=122))
+{
+printf("\n string element is not numeric:");
+}
+else
+{
+printf("\n string element is numeric:");
+}
+return 0;
+}
+
+
+
+
+
