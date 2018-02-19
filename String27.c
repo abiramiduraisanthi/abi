39bb0b7ffe21e113853cce240d87a7880ec2b909
abiramiduraisanthi <31506090+abiramiduraisanthi@users.noreply.github.com>
2018-02-19 10:28:38 +0000

    Update String27.c

--- String27.c
+++ String27.c
@@ -4,7 +4,7 @@ int main()
 char n;
 printf("\n enter the string element:");
 scanf("%d",&n);
-if((n>=65&&n<=90)<||(n>=97&& n<=122))
+if((n>=65&&n<=90)||(n>=97&&n<=122))
 {
 printf("\n string element is not numeric:");
 }
