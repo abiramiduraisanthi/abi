a7f78c226f4399a79bdd2421f02796d0756f5428
abiramiduraisanthi <31506090+abiramiduraisanthi@users.noreply.github.com>
2018-02-19 11:04:47 +0000

    Update Pro21.c

--- Pro21.c
+++ Pro21.c
@@ -1,18 +1,16 @@
 #include<stdio.h>
 int main()
 {
-int i,j,n=0,t,sum=0;
-printf("\n enter the first element:");
-scanf("%d",&a)
-printf("\n enter the difference \n enter the count:")
-scanf("%d%d",&d,&n);
+int a,d,n,i,sum=0;
+printf("\n enter the count:");
+scanf("%d",&n);
+printf("\n enter the difference \n enter the first element a:");
+scanf("%d%d",&d,&a);
 for(i=0;i<=n;i++)
 {
-if(t==n)
-{
-sum=sum+i;
+sum=sum+a;
+a=a+d;  
 }
 printf("%d",sum);
-}
 return 0;
 }
