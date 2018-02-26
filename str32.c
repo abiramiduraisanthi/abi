faf894a4976703455d57b2daf84e3a90249a1181
abiramiduraisanthi <31506090+abiramiduraisanthi@users.noreply.github.com>
2018-02-26 15:49:14 +0530

    Create str32.c

---
+++ str32.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char string[100];
+int i,count=1;
+printf("\n enter the string:");
+scanf("%[^\n]",&string);
+for(i=0;string[i]!='\0';i++)
+{
+if(string[i]==' ')
+{
+count++;
+}
+}
+printf("%d",count);
+return 0;
+}
