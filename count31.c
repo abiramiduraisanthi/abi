ad983bf3b1e00076287608d2917e7b75bbd49277
abiramiduraisanthi <31506090+abiramiduraisanthi@users.noreply.github.com>
2018-02-26 15:57:44 +0530

    Update count31.c

--- count31.c
+++ count31.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
 int main()
 {
-char a[20];
-int i;
+char string[100];
+int i,count=0;
 printf("\n enter the string:");
-scanf("%s",&a);
-for(i=0;a[i]!='\0';++i)
+scanf("%[^\n]",&string);
+for(i=0;string[i]!='\0';i++)
 {
-printf("%d\n total string in character:",i);
+if(string[i]>'0'&&string[i]<='9 ')
+{
+count++;
 }
+}
+printf("%d",count);
 return 0;
 }
+
