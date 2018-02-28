0a6ba4fc742039fafa086fc151e10b7eff9c925e
abiramiduraisanthi <31506090+abiramiduraisanthi@users.noreply.github.com>
2018-02-28 16:57:10 +0530

    Create sk41.c

---
+++ sk41.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char s[10];
+int i,n;
+printf("\n enter the string:");
+scanf("%s",&s);
+printf("\n enter the number:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("%s\n",s);
+}
+return 0;
+}
