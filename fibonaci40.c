47036777519fcd597955c116eb1e8afc5dd0ce00
abiramiduraisanthi <31506090+abiramiduraisanthi@users.noreply.github.com>
2018-02-28 16:46:00 +0530

    Update fibonaci40.c

--- fibonaci40.c
+++ fibonaci40.c
@@ -3,10 +3,10 @@ int main()
 {
 int t1=1,t2=1,nt,n,i;
 printf("\n enter the number:");
-scanf(" %d ",&n);
+scanf("%d",&n);
 for(i=0;i<n;i++)
 {
-printf("%d",t1);
+printf(" %d ",t1);
 nt=t1+t2;
 t1=t2;
 t2=nt;
