4893fdabf0264a59ec491438aefb0165c401849d
abiramiduraisanthi <31506090+abiramiduraisanthi@users.noreply.github.com>
2018-03-12 16:42:20 +0530

    Update evenum.c

--- evenum.c
+++ evenum.c
@@ -6,14 +6,10 @@ printf("\n enter the two intervels:");
 scanf("%d%d",&f,&l);
 for(i=f;i<=l;i++)
 {
-if(i%2==1)
+if(i%2==0)
 {
-printf("%d\n enter the intervels are even number:",i);
-}
-else
-{
-printf("%d\n enter the intervels are not even number:",i);
+printf("%d\n  even number:",i);
 }
 }
-return 0;
+  return 0;
 }
