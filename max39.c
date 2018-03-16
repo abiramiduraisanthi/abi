3413bc369e738ebead767f0413014d01dbf8af4c
abiramiduraisanthi <31506090+abiramiduraisanthi@users.noreply.github.com>
2018-03-16 16:26:06 +0530

    Update max39.c

--- max39.c
+++ max39.c
@@ -4,11 +4,14 @@ int main()
 int i,max,a[10];
 printf("\n enter 10 numbers:");
 scanf("%d",&a[0]);
-max=a[0];
 for(i=1;i<=10;i++)
 {
 scanf("%d",&a[i]);
+}
+  max=a[0];
+  for(i=1;i<=10;i++)
 if(a[i]>max)
+{
 max=a[i];
 }
 printf("%d",max);
